7e6115c26361427074b8d7e966402f52bf44119e
kewablay <kojokewajunior123@gmail.com>
2024-06-24 07:30:32 +0000

    feat: added task step 3 and 4

--- index.js
+++ index.js
@@ -1,21 +1,50 @@
-const superhero = {
-  name: "Spider-Man",
-  secretIdentity: "Peter Parker",
-  powers: ["wall-crawling", "super strength", "spider-sense"],
-  weakness: "Ethyl Chloride Pesticide",
-  usePower: function (powerName) {
+// const superhero = {
+//   name: "Spider-Man",
+//   secretIdentity: "Peter Parker",
+//   powers: ["wall-crawling", "super strength", "spider-sense"],
+//   weakness: "Ethyl Chloride Pesticide",
+//   usePower: function (powerName) {
+//     if (this.powers.includes(powerName)) {
+//       console.log(`${this.name} is using ${powerName}!`);
+//     } else {
+//       console.log(`${this.name} doesn't have the power: ${powerName}`);
+//     }
+//   },
+//   revealIdentity: function () {
+//     console.log(
+//       `The secret identity of ${this.name} is ${this.secretIdentity}`
+//     );
+//   },
+// };
+
+// superhero.usePower("super strength");
+// superhero.revealIdentity();
+
+
+// step 3
+function Superhero(name, secretIdentity, powers, weakness) {
+    this.name = name;
+    this.secretIdentity = secretIdentity;
+    this.powers = powers;
+    this.weakness = weakness;
+}
+
+const batman = new Superhero("Batman", "Bruce Wayne", ["martial arts", "gadgets"], "None");
+console.log(batman);
+
+
+
+Superhero.prototype.usePower = function(powerName) {
     if (this.powers.includes(powerName)) {
-      console.log(`${this.name} is using ${powerName}!`);
+        console.log(`${this.name} is using ${powerName}!`);
     } else {
-      console.log(`${this.name} doesn't have the power: ${powerName}`);
+        console.log(`${this.name} doesn't have the power: ${powerName}`);
     }
-  },
-  revealIdentity: function () {
-    console.log(
-      `The secret identity of ${this.name} is ${this.secretIdentity}`
-    );
-  },
 };
 
-superhero.usePower("super strength");
-superhero.revealIdentity();
+Superhero.prototype.revealIdentity = function() {
+    console.log(`The secret identity of ${this.name} is ${this.secretIdentity}`);
+};
+
+batman.usePower("gadgets");
+batman.revealIdentity();
